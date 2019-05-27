587ae3c23af154d8320fd222f2eb74a3170ef91c
Malik <kyli49@hotmail.com>
2019-05-27 17:50:40 +0200

    Python App 02 GUESS NUMBER GAME

---
+++ 02_guess_number_game/program.py
@@ -0,0 +1,23 @@
+import random
+
+print('--------------------------------')
+print('     GUESS NUMBER GAME')
+print('--------------------------------')
+
+the_number = random.randint(0, 100)
+
+guss = -1
+
+name = input('(Player) what is your name: ')
+while guss != the_number:
+    guss_text = input('Guss a number between 0 and 100:  ')
+    guss = int(guss_text)
+
+    if guss < the_number:
+        print('Sorry {1}, your guss of {0} was too low, '.format(guss, name))
+    elif guss > the_number:
+        print('Sorry {1}, your guss of {0} was too high, '.format(guss, name))
+    else:
+        print('Hora {1},  you win!'.format(guss, name))
+
+print('done')
