6ed4394cc63a63c4e84dbd39fcca02060d2f22ec
Malik <kyli49@hotmail.com>
2019-05-27 16:55:16 +0200

    Python app 1 and app 2

---
+++ 01_hello_world/program.py
@@ -0,0 +1,9 @@
+print("--------------------------")
+print(             "Hello world")
+print("--------------------------")
+
+
+user_input = input('please insert your name? ')
+greeting = ' Nice to meet you ' + user_input
+
+print(greeting)
