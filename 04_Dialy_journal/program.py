ed2a62c4afaff406f8be0132c3f3a7c5dcce98b7
Malik <kyli49@hotmail.com>
2019-11-14 10:44:03 +0100

    fix

---
+++ 04_Dialy_journal/program.py
@@ -0,0 +1,27 @@
+def main():
+    print_header()
+    run_event_loop()
+
+
+def print_header():
+    print('-----------------------------')
+    print('            Journal')
+    print('-----------------------------')
+    print()
+
+
+def run_event_loop():
+    print('What you. going to do with your journal?')
+
+    cmd == None
+
+    while cmd !='x':
+        cmd = input('[L]ist entries, [A]dd entry, E[x]ite:')
+
+        if cmd == 'L':
+            print("L")
+        elif cmd == 'A':
+            print("A")
+
+
+main()
